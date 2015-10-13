fccc6e1ce22781004e9fd2f73b16ac4955c41e4a
conekilla69 <wrcfan@verizon.net>
2015-10-13 06:12:10 +0000

    could not figure out how to make these loop.

---
+++ task_01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-*- coding: utf-8: -*-
+""""This will create hoepfully create player match ups."""
+
+
+PLAYER = ['James', 'importJesse', 'Jenn'] 
+def get_matches(PLAYER):
+    PLAYER = ['James', 'Jesse', 'Jenn'] 
+    for myind, myitem in enumerate(PLAYER):
+        PLAYER [myind] = myitem[1]
+    print  myind, myitem
+    
---
+++ task_02.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+"""Signon with a password."""
+
+
+import authentication.getpass
+
+
+valid == False
+
+def login(username, maxattempts =3):
+    """"Set parameters.
+    username (str): string representing the username to log in with.
+    maxattempts (int): number of attempts allowed, defaults to 3.
+    """
+print 'Password'
+
+while not valid:
+    strinput = getpass.getpass('username')
+    print strinput
+    if strinput = authentication.authenticate()
+    not valid = True
+    print 'welcome'
+    else:
+        print 'Please try again'
+        retun login()
+         
+        
+    
+    
+    
+
+
+
+    
